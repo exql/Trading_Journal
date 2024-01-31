addfd39e559869d217d16335453c8fe3f96c850e
exql <exl_88@hotmail.com>
2024-01-31 13:44:24 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Docker_template
+Docker template for python_Django_Mysql
---
+++ initial.sql
@@ -0,0 +1,5 @@
+-- -----------------------------------------------------
+-- Schema django
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `Trading_Assistanst` DEFAULT CHARACTER SET UTF8;
+USE `Trading_Assistanst`;
